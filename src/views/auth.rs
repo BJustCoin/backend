4b8c95309811cf57665fc30c958aa5d6d475d346
interesnij <ochkarik1983@mail.ru>
2024-12-25 01:12:52 +0300

    J

--- src/views/auth.rs
+++ src/views/auth.rs
@@ -133,7 +133,7 @@ async fn invite(body: web::Json<EmailUserReq>) -> Result<HttpResponse, ApiError>
 
     let mail_info = sendgrid::Mail::new()
         .add_to(sendgrid::Destination {
-            address: body.email,
+            address: &body.email,
             name: "BJustCoin Team",
         })
         .add_from("no-reply@bjustcoin.com")
