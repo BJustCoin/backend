f285d24cc2388b7c4786e3b4a7e8ebc698aa103f
interesnij <ochkarik1983@mail.ru>
2024-12-25 13:34:22 +0300

    J

--- src/views/auth.rs
+++ src/views/auth.rs
@@ -73,7 +73,7 @@ async fn invite(body: web::Json<EmailUserReq>) -> Result<HttpResponse, ApiError>
     let mut x_smtpapi = String::new();
     x_smtpapi.push_str(r#"{"unique_args":{"test":7}}"#);
 
-    let text = "Our confirmation code - <strong>".to_owned() + &token_string.to_string() + &"</strong>".to_string();
+    let text = "Our confirmation code - <strong>".to_owned() + &token.id.to_string() + &"</strong>".to_string();
     let mail_info = sendgrid::Mail::new()
         .add_to(sendgrid::Destination {
             address: &body.email,
