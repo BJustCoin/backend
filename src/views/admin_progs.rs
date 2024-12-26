9a347367e56040bac5619110284a061bb1129a31
interesnij <ochkarik1983@mail.ru>
2024-12-26 14:45:32 +0300

    J

--- src/views/admin_progs.rs
+++ src/views/admin_progs.rs
@@ -25,7 +25,7 @@ pub fn admin_routes(config: &mut web::ServiceConfig) {
     config.route("/get_users/", web::get().to(get_users));
     config.route("/get_admins/", web::get().to(get_admins));
     config.route("/get_banned_users/", web::get().to(get_banned_users));
-    config.route("/get_banned_admins/", web::get().to(get_banned_admins));
+    config.route("/get_banned_admins/", web::get().to(get_banned_admins)); 
 
     config.route("/block_user/", web::post().to(block_user));
     config.route("/unblock_user/", web::post().to(unblock_user));
@@ -58,11 +58,11 @@ pub async fn get_users(session: Session, req: HttpRequest) -> Json<AuthRespData>
         let page = crate::utils::get_page(&req);
         let _request_user = get_current_user(&session).expect("E.");
         Json(_request_user.get_users_list(page.into(), Some(20)))
-    }
+    } 
     else {
         Json(AuthRespData {
             data: Vec::new(),
-            next: 0,
+            next: 10,
         })
     }
 }
