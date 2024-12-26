55673cb3e75f57dcf9259aa83befae173897880f
interesnij <ochkarik1983@mail.ru>
2024-12-26 11:46:21 +0300

    J

--- src/views/admin_progs.rs
+++ src/views/admin_progs.rs
@@ -57,7 +57,7 @@ pub async fn get_users(session: Session, req: HttpRequest) -> Json<AuthRespData>
     if is_signed_in(&session) {
         let page = crate::utils::get_page(&req);
         let _request_user = get_current_user(&session).expect("E.");
-        Json(_request_user.get_users_list(page.into(), 20))
+        Json(_request_user.get_users_list(page.into(), Some(20)))
     }
     else {
         Json(AuthRespData {
@@ -70,7 +70,7 @@ pub async fn get_admins(session: Session, req: HttpRequest) -> Json<AuthRespData
     if is_signed_in(&session) {
         let page = crate::utils::get_page(&req);
         let _request_user = get_current_user(&session).expect("E.");
-        Json(_request_user.get_admins_list(page.into(), 20))
+        Json(_request_user.get_admins_list(page.into(), Some(20)))
     }
     else {
         Json(AuthRespData {
@@ -83,7 +83,7 @@ pub async fn get_banned_users(session: Session, req: HttpRequest) -> Json<AuthRe
     if is_signed_in(&session) {
         let page = crate::utils::get_page(&req);
         let _request_user = get_current_user(&session).expect("E.");
-        Json(_request_user.get_banned_users_list(page.into(), 20))
+        Json(_request_user.get_banned_users_list(page.into(), Some(20)))
     }
     else {
         Json(AuthRespData {
@@ -96,7 +96,7 @@ pub async fn get_banned_admins(session: Session, req: HttpRequest) -> Json<AuthR
     if is_signed_in(&session) {
         let page = crate::utils::get_page(&req);
         let _request_user = get_current_user(&session).expect("E.");
-        Json(_request_user.get_banned_admins_list(page.into(), 20))
+        Json(_request_user.get_banned_admins_list(page.into(), Some(20)))
     }
     else {
         Json(AuthRespData {
